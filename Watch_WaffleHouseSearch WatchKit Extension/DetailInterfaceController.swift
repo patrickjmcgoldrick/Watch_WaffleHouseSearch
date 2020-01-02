f43ccc41cbde563ec44d61b8bc6975082c623575
Me <Me>
2020-01-02 00:56:18 -0800

    feat: building out UI

---
+++ Watch_WaffleHouseSearch WatchKit Extension/DetailInterfaceController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailInterfaceController.swift
+//  Watch_WaffleHouseSearch WatchKit Extension
+//
+//  Created by dirtbag on 1/1/20.
+//  Copyright © 2020 dirtbag. All rights reserved.
+//
+
+import UIKit
+
+class DetailInterfaceController: WKInterfaceController {
+
+}
---
+++ Watch_WaffleHouseSearch WatchKit Extension/TableInterfaceController.swift
@@ -0,0 +1,13 @@
+//
+//  TableInterfaceController.swift
+//  Watch_WaffleHouseSearch WatchKit Extension
+//
+//  Created by dirtbag on 1/1/20.
+//  Copyright © 2020 dirtbag. All rights reserved.
+//
+
+import UIKit
+
+class TableInterfaceController: WKInterfaceController {
+
+}
